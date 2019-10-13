b71c5353d2493d4d0edd8424d7c6f353c6a5757f
ihokamura <ihokamura.contact@gmail.com>
2019-10-14 07:42:07 +0900

    remove unnecessary spaces

--- sklearn_l1_regularization.py
+++ sklearn_l1_regularization.py
@@ -20,7 +20,7 @@ def main():
     # split sample data into training data and test data and standardize them
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1, stratify=y)
     sc = StandardScaler().fit(X_train)
-    X_train_std = sc.transform(X_train)                                                                         
+    X_train_std = sc.transform(X_train)
     X_test_std = sc.transform(X_test)
 
     # fit classifiers
