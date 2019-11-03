93620902de326d5ef03351448d4c33f7b94c283e
ihokamura <ihokamura.contact@gmail.com>
2019-11-03 10:53:28 +0900

    show usage of LinearRegression of scikit-learn

---
+++ sklearn_linear_regression.py
@@ -0,0 +1,40 @@
+"""
+show sample of how to use LinearRegression
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+from data_utility import HousingData
+from plot_utility import plot_predictions
+
+
+def main():
+    # prepare training data and target variable
+    features = ['RM']
+    D = HousingData(features)
+    X, y = D.X, D.y
+
+    # fit regressors
+    regressors = [
+        LinearRegression().fit(X, y)
+    ]
+
+    for regressor in regressors:
+        # show prediction
+        plot_predictions(
+            X, y, regressor,
+            xlabel='RM', ylabel='MEDV')
+
+        # show a sample of non-standardized prediction and weights
+        rm = [[5.0]]
+        medv = regressor.predict(rm)[0]
+        print('RM = 5.0 -> MEDV = {:.3e}'.format(medv))
+
+        # show weights
+        print('intercept = {i:.3e}, slope = {s:.3e}'.format(i=regressor.intercept_, s=regressor.coef_[0]))
+
+
+if __name__ == '__main__':
+    main()
