04d89737ee07427d312174521da29ce9b70b247f
ihokamura <ihokamura.contact@gmail.com>
2019-11-10 16:39:20 +0900

    show usage of KMeans of scikit-learn

---
+++ sklearn_kmeans.py
@@ -0,0 +1,63 @@
+"""
+show sample of how to use KMeans
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.datasets import make_blobs
+
+
+def main():
+    # prepare sample data
+    centers = 3
+    X, _ = make_blobs(
+        n_samples=150,
+        n_features=2,
+        centers=centers,
+        cluster_std=0.5,
+        shuffle=True,
+        random_state=0)
+
+    # fit clusterings
+    clusterings = [
+        KMeans(
+            n_clusters=3,
+            init='random',
+            n_init=10,
+            max_iter=300,
+            tol=1.0e-4,
+            random_state=1),
+        KMeans(
+            n_clusters=3,
+            init='k-means++',
+            n_init=10,
+            max_iter=300,
+            tol=1.0e-4,
+            random_state=1)
+    ]
+    names = ['k-means', 'k-means++']
+
+    for clustering, name in zip(clusterings, names):
+        # predict centroid of clusters and label of each data points
+        y_pred = clustering.fit_predict(X)
+        # plot predicted labels
+        for i in range(centers):
+            Xi = X[y_pred == i]
+            plt.scatter(
+                Xi[:, 0], Xi[:, 1],
+                marker='o', edgecolor='black', label='cluster {0}'.format(i + 1))
+        # plot centroids
+        plt.scatter(
+            clustering.cluster_centers_[:, 0], clustering.cluster_centers_[:, 1],
+            marker='*', edgecolor='black', label='centroids')
+        # set plot area
+        plt.grid()
+        plt.legend()
+        plt.title(name)
+        plt.tight_layout()
+        plt.show()
+
+
+if __name__ == '__main__':
+    main()
