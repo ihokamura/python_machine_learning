a9df2b21ca87bd60b5271f4b8d0d136ca44c38f6
ihokamura <ihokamura.contact@gmail.com>
2019-11-23 16:27:21 +0900

    add elbow method and silhouette analysis

--- sklearn_kmeans.py
+++ sklearn_kmeans.py
@@ -2,13 +2,21 @@
 show sample of how to use KMeans
 """
 
+from matplotlib import cm
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.datasets import make_blobs
+from sklearn.metrics import silhouette_samples
 
 
 def main():
+    show_clustering()
+    show_elbow()
+    show_silhouette()
+
+
+def show_clustering():
     # prepare sample data
     centers = 3
     X, _ = make_blobs(
@@ -55,6 +63,92 @@ def main():
         plt.grid()
         plt.legend()
         plt.title(name)
+        plt.tight_layout()
+        plt.show()
+
+        # show attributes
+        print('inertia:{0}'.format(clustering.inertia_))
+        print('iteration times:{0}'.format(clustering.n_iter_))
+
+
+def show_elbow():
+    # prepare sample data
+    centers = 3
+    X, _ = make_blobs(
+        n_samples=150,
+        n_features=2,
+        centers=centers,
+        cluster_std=0.5,
+        shuffle=True,
+        random_state=0)
+
+    # compute inertia for different number of clusters
+    n_clusters_range = range(1, 11)
+    inertia = []
+    for n_clusters in n_clusters_range:
+        # fit clustering
+        clustering = KMeans(
+            n_clusters=n_clusters,
+            init='k-means++',
+            n_init=10,
+            max_iter=300,
+            tol=1.0e-4,
+            random_state=1)
+        clustering.fit(X)
+        inertia.append(clustering.inertia_)
+
+    # plot inertia
+    plt.plot(n_clusters_range, inertia, marker='o')
+    plt.xlabel('number of clusters')
+    plt.ylabel('inertia')
+    plt.tight_layout()
+    plt.show()
+
+
+def show_silhouette():
+    # prepare sample data
+    centers = 3
+    X, _ = make_blobs(
+        n_samples=150,
+        n_features=2,
+        centers=centers,
+        cluster_std=0.5,
+        shuffle=True,
+        random_state=0)
+
+    for n_clusters in range(2, 5):
+        # fit clustering and predict labels
+        clustering = KMeans(
+            n_clusters=n_clusters,
+            init='k-means++',
+            n_init=10,
+            max_iter=300,
+            tol=1.0e-4,
+            random_state=1)
+        y = clustering.fit_predict(X)
+
+        # compute silhouette coefficients
+        cluster_labels = np.unique(y)
+        n_clusters = len(cluster_labels)
+        silhouette_coefficients = silhouette_samples(X, y, metric='euclidean')
+
+        # show silhouette plot
+        yticks = []
+        y_lower, y_upper = 0, 0
+        for i, c in enumerate(cluster_labels):
+            values = np.sort(silhouette_coefficients[y == c])
+            color = cm.jet(i / n_clusters)
+            y_upper += len(values)
+            plt.barh(
+                range(y_lower, y_upper), values,
+                height=1.0, color=color, edgecolor='none')
+            yticks.append((y_lower + y_upper) / 2)
+            y_lower += len(values)
+        plt.axvline(np.mean(silhouette_coefficients), color='black', linestyle='--')
+        plt.yticks(yticks, cluster_labels + 1)
+        plt.title('k = {0}'.format(n_clusters))
+        plt.xlabel('silhouette coefficient')
+        plt.ylabel('cluster')
         plt.tight_layout()
         plt.show()
 
